0307d970335a54511285a0a2d879dd5da96decd3
Joseph <jaathunder@yahoo.com>
2019-10-28 13:52:45 -0700

    Add and implement rounding method properly, mostly complete

--- src/Main/Item.java
+++ src/Main/Item.java
@@ -8,9 +8,11 @@ class Item {
     Item() {
         //can optionally have no attributes when initializing
     }
+
     Item(double p) {
         price = p;
     }
+
     Item(double p, double w) {
         price = p;
         weight = w;
@@ -19,14 +21,47 @@ class Item {
     void addPrice(double p) {
         price = p;
     }
+
     double getPrice() {
         return price;
     }
+
     void addWeight(double w) {
         weight = w;
     }
+
     double getWeight() {
-        return weight;
+        return round(weight, 3);
     }
 
+    double getPricePerOZ() {
+        if (price != 0) {
+            if (weight != 0) {
+                return round((price / weight), 2);
+            }else{
+                return 0;
+            }
+        }else{
+            return 0;
+        }
+    }
+
+    //implement my own rounding method for reusability
+    //however, this can only be in each class, not in the main method (needs more work here)
+    //TODO: find a way to remove the .0 off of integer double values, cannot output a different data value
+    //also strings are annoying to handle
+    public static double round(double i, int p) {
+        //multiply by places, cast to int, divide by places
+        //i = input, p = decimal places, 0 or larger
+
+        //get places var to work
+        double places = 1;
+        if (p > 0) {
+            for (int j = p; j > 0; j--) {
+                places = places * 10;
+            }
+        }
+
+        return ((int)((i * places) + 0.5)) / places;
+    }
 }
--- src/Main/Store.java
+++ src/Main/Store.java
@@ -1,15 +1,27 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Store {
 
     public static void main(String[] args) {
 
-        Item apple = new Item();
+        Scanner kb = new Scanner(System.in);
+
+        Item apple = new Item(0.99, 5.4);
         apple.addPrice(0.99);
 
         System.out.println("Apples are: $" + apple.getPrice());
-        System.out.println("Apples weigh " + apple.getWeight() + "oz on average.");
+
+        System.out.println("How much do apples weigh?");
+        apple.addWeight(kb.nextDouble());
+
+        System.out.println("Apples weigh " + apple.getWeight() + " oz on average.");
+
+        System.out.println("Apples cost $" + apple.getPricePerOZ() + " per oz.");
 
     }
+
+
 
 }
